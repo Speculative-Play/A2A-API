42e7dd4b4f71cbfbbae7b6b90eef6ae64dbae26f
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2022-10-25 13:11:01 -0400

    Update starred_match_profiles#index and authentication

--- app/controllers/api/v1/starred_match_profiles_controller.rb
+++ app/controllers/api/v1/starred_match_profiles_controller.rb
@@ -1,13 +1,19 @@
 class Api::V1::StarredMatchProfilesController < ApplicationController
-  before_action :authenticate_parent_profile
+  before_action :current_account
 
   # GET /starred_match_profiles
   def index
-    # Returns all starred_match_profiles that share parent_profile_id
-    @starred_match_profiles = if params[:parent_profile_id].present?
-      StarredMatchProfile.where("parent_profile_id = ?", params[:parent_profile_id])
+    puts "insie starred_match_profile controller > index"
+    puts "now will check for a current user"
+    if !current_user_profile.nil?
+      @starred_match_profiles = StarredMatchProfile.where(parent_profile_id: ParentProfile.find_by(user_profile_id: @current_user_profile))
+      render json: @starred_match_profiles
+    elsif !current_parent_profile.nil?
+      @starred_match_profiles = StarredMatchProfile.where(parent_profile_id: @current_parent_profile)
+      render json: @starred_match_profiles
+    else
+      render json: "must login"
     end
-    render json: @starred_match_profiles
   end
 
   # GET /starred_match_profiles/1
@@ -15,7 +21,7 @@ class Api::V1::StarredMatchProfilesController < ApplicationController
     render json: @starred_match_profile
   end
 
-  # POST /starred_match_profiles/[parent_profile_id]
+  # POST /starred_match_profiles
   def create
     @starred_match_profile = StarredMatchProfile.new(starred_match_profile_params)
     if @starred_match_profile.save
@@ -25,14 +31,14 @@ class Api::V1::StarredMatchProfilesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /starred_match_profiles/1
-  def update
-    if @starred_match_profile.update(starred_match_profile_params)
-      render json: @starred_match_profile
-    else
-      render json: @starred_match_profile.errors, status: :unprocessable_entity
-    end
-  end
+  # # PATCH/PUT /starred_match_profiles/1
+  # def update
+  #   if @starred_match_profile.update(starred_match_profile_params)
+  #     render json: @starred_match_profile
+  #   else
+  #     render json: @starred_match_profile.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /starred_match_profiles/1
   def destroy
@@ -40,17 +46,9 @@ class Api::V1::StarredMatchProfilesController < ApplicationController
   end
 
   private
-    def authenticate_parent_profile
-      if logged_in_parent_profile?
-        puts "inside parent_profile > authenticate_parent_profile > logged in!"
-      else
-        puts "inside parent_profile > authenticate_parent_profile > login unsuccessful!"
-      end
-    end
-
-    # Only allow a list of trusted parameters through.
-    def starred_match_profile_params
-      # params.fetch(:starred_match_profile, {})
-      params.require(:starred_match_profile).permit(:parent_profile_id, :match_profile_id)
-    end
+  # Only allow a list of trusted parameters through.
+  def starred_match_profile_params
+    # params.fetch(:starred_match_profile, {})
+    params.require(:starred_match_profile).permit(:parent_profile_id, :match_profile_id)
+  end
 end
