3297737ce5bc06d7c3482b195e0767ea387d70ac
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2022-10-27 16:51:30 -0400

    Update user_question_answers#index formatting algo

--- app/controllers/api/v1/user_question_answers_controller.rb
+++ app/controllers/api/v1/user_question_answers_controller.rb
@@ -2,14 +2,28 @@ class Api::V1::UserQuestionAnswersController < ApplicationController
   before_action :current_account
 
 
-  # GET /user_question_answers
+  # GET /my_question_answers
   def index
     if !current_user_profile.nil?
-      @user_question_answers = UserQuestionAnswer.where(user_profile_id: @current_account.user_profile)
-      @questions = Question.joins(:user_question_answers).where("user_question_answers.user_profile_id" => @current_account.user_profile)
-      @answers = Answer.joins(:user_question_answers).where("user_question_answers.user_profile_id" => @current_account.user_profile)
-
-      render json: {all_data: {user_question_answers: @user_question_answers, questions: @questions, answers: @answers}}
+      @categories_arr = Hash.new
+      MatchmakingCategory.find_each do |match_cat|
+        @questions = Question.where(matchmaking_category_id: match_cat.id)
+        questions_arr = Hash.new
+        @questions.each do |q|
+          @answers = Answer.where(question_id: q.id)
+          answers_arr = Hash.new
+          @answers.each do |a|
+            if UserQuestionAnswer.where("question_id = ? AND answer_id = ? AND user_profile_id = ?", q.id, a.id, current_user_profile.id).present?
+              answers_arr[a.answer_text] = [1]
+            else
+              answers_arr[a.answer_text] = [0]
+            end
+          end
+          questions_arr[q.question_text] = [answers_arr]
+        end
+        @categories_arr[match_cat.category_name] = [questions_arr]
+      end
+      render json: {user_question_answers: @categories_arr}
     else
       return head(:unauthorized)
     end
