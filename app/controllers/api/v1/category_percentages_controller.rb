0afce9070e0d857cb9b354bd893f7ecc9dfbd9af
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2022-10-31 14:27:47 -0400

    Update category_percentages#index and update

--- app/controllers/api/v1/category_percentages_controller.rb
+++ app/controllers/api/v1/category_percentages_controller.rb
@@ -5,10 +5,13 @@ class Api::V1::CategoryPercentagesController < ApplicationController
   def index
     if !current_user_profile.nil?
       @category_percentages = CategoryPercentage.where(user_profile_id: @current_user_profile)
-      render json: @category_percentages
+    elsif !current_parent_profile.nil?
+      @category_percentages = CategoryPercentage.where(parent_profile_id: @current_parent_profile)
     else
       return head(:unauthorized)
     end
+      
+    render json: @category_percentages
   end
 
   # GET /category_percentages/1
@@ -30,31 +33,34 @@ class Api::V1::CategoryPercentagesController < ApplicationController
   # PATCH/PUT /category_percentages
   def update
     if !current_user_profile.nil?
-
-      # parse JSON and create variables
-      @json = JSON.parse(request.body.read)
-      @input_category_id_array = []               # input matchmaking_category_ids
-      @input_category_percentage_array = []       # input category_percentages
-
       @current_category_percentages = CategoryPercentage.where("user_profile_id = ?", @current_user_profile.id)
-
-      # collect and store the input values in arrays
-      @json['category_percentages_input'].each_with_index do |category, index|
-        @input_category_id_array[index] = category['matchmaking_category_id']
-        @input_category_percentage_array[index] = category['category_percentage']
-      end
-
-      # TODO: do we need to implement verification that total value of all category_percentages = 100%)? Or done on frontend?
-
-      # update the records with the new values
-      @input_category_id_array.each_with_index do |array, index|
-        CategoryPercentage.update(@current_category_percentages[index].id, :category_percentage => @input_category_percentage_array[index])
-      end
-
-      index
+    elsif !current_parent_profile.nil?
+      @current_category_percentages = CategoryPercentage.where("parent_profile_id = ?", @current_parent_profile.id)
     else
       return head(:unauthorized)
     end
+
+
+    # parse JSON and create variables
+    @json = JSON.parse(request.body.read)
+    @input_category_id_array = []               # input matchmaking_category_ids
+    @input_category_percentage_array = []       # input category_percentages
+
+
+    # collect and store the input values in arrays
+    @json['category_percentages'].each_with_index do |category, index|
+      @input_category_id_array[index] = category['matchmaking_category_id']
+      @input_category_percentage_array[index] = category['category_percentage']
+    end
+
+    # TODO: do we need to implement verification that total value of all category_percentages = 100%)? Or done on frontend?
+
+    # update the records with the new values
+    @input_category_id_array.each_with_index do |array, index|
+      CategoryPercentage.update(@current_category_percentages[index].id, :category_percentage => @input_category_percentage_array[index])
+    end
+
+    index
   end
 
   # DELETE /category_percentages/1
