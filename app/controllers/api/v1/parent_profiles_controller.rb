6b6667a2df026b455caca753126ce593dd70df5b
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2022-12-20 13:11:05 -0500

    update search_child in parent_profile_controller

--- app/controllers/api/v1/parent_profiles_controller.rb
+++ app/controllers/api/v1/parent_profiles_controller.rb
@@ -58,14 +58,17 @@ class Api::V1::ParentProfilesController < ApplicationController
   end
 
   def search_child
-    if Account.exists?(email: params[:parent_profile][:child_email])
-      if !Account.find_by(email: params[:parent_profile][:child_email]).user_profile.nil?
-        return true
+    puts params[:child_email]
+    # if Account.exists?(email: params[:parent_profile][:child_email])
+    if Account.exists?
+      if !Account.find_by(email: params[:child_email]).nil?
+        @user = Account.find_by(email: params[:child_email]).user_profile
+        render json: @user
       else
         return false
       end
     else
-      return false
+      render json: []
     end
   end
 
