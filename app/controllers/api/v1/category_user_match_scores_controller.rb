a83aea6bdbd7ee0d368d76f2f1d93e05958b7abd
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2023-01-26 16:53:56 -0500

    Add Create methods for score controllers

--- app/controllers/api/v1/category_user_match_scores_controller.rb
+++ app/controllers/api/v1/category_user_match_scores_controller.rb
@@ -16,8 +16,24 @@ class Api::V1::CategoryUserMatchScoresController < ApplicationController
   
     # POST /create_category_score
     def create
-
-      
+      if !current_account.nil?
+        # called whenever a new user_profile is created
+        # for every match_profile
+        current_user = current_account.user_profile
+        MatchProfile.find_each do |match_profile|
+          MatchmakingCategory.find_each do |matchmaking_category|
+            category_percentage = CategoryPercentage.where(user_profile_id: current_user.id).where(matchmaking_category_id: matchmaking_category.id)
+            # create a category match score with current_user and every match_profile
+            category_user_match_score = CategoryUserMatchScore.new(user_profile_id: current_user.id, match_profile_id: match_profile.id, matchmaking_category_id: matchmaking_category.id, category_percentage: category_percentage.category_percentage)
+            if category_user_match_score.save
+              next
+            else
+              render json: category_user_match_score.errors, status: :unprocessable_entity
+            end
+        end
+      else
+        return head(:unauthorized)
+      end
     end
   
     # PATCH/PUT /category_user_match_score/1
--- app/controllers/api/v1/global_user_match_scores_controller.rb
+++ app/controllers/api/v1/global_user_match_scores_controller.rb
@@ -14,10 +14,24 @@ class Api::V1::GlobalUserMatchScoresController < ApplicationController
       end
     end
   
-    # POST /create_category_score
+    # POST /create_global_score
     def create
-
-      
+      if !current_account.nil?
+        # called whenever a new user_profile is created
+        # for every match_profile
+        current_user = current_account.user_profile
+        MatchProfile.find_each do |match_profile|
+          # create a global match score with current_user and every match_profile
+          global_user_match_score = GlobalUserMatchScore.new(user_profile_id: current_user.id, match_profile_id: match_profile.id)
+          if global_user_match_score.save
+            next
+          else
+            render json: global_user_match_score.errors, status: :unprocessable_entity
+          end
+        end
+      else
+        return head(:unauthorized)
+      end
     end
   
     # PATCH/PUT /category_user_match_score/1
